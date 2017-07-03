fbb9f1392d0e43b091101efbf51869ba3b3032d6
hmtavares <hmtavares@users.noreply.github.com>
2017-07-03 15:33:19 -0400

    Upd readme (#1)
    * Update the readme with some basic documentation
    
    * read me markdown edits
    
    * Add code file and gitignore

--- README.md
+++ README.md
@@ -1,2 +1,179 @@
-# dod_trainer
+# Deduce or Die trainer
 A solitaire trainer for Deduce or Die
+
+Deduce or Die is a deductive card game where players each have a hand of cards and try and figure out who has a specific card.
+
+Some handy references
+* [Rules for Deduce or Die](http://www.thegamesjournal.com/rules/DeduceOrDie.shtml)
+* [Deduce or Die Boardgame Geek page](https://boardgamegeek.com/boardgame/19765/deduce-or-die)
+
+# Origin
+I was introduced to the game by some friends and was terrible at it. I decided I needed to "practice". I cobbled together this solitaire version of DoD to facilitate practicing.
+
+The core game mechanics are completly formalized and don't require any human interaction. You "ask" another player a formal question using one of the following forms:
+* How many cards do you have between X & Y of any suit
+* How many cards do you have between X & Y of a specific suit
+
+Normally this would rotate around the table, each player asking a question. In this solitaire format the single player asks all the questions of the other (computer) players. Not as fun but definitly aids in figuring out what kinds of questions to ask, how to record the question and the response and how to use the information to deduce the final answer.
+
+# Requirements
+* Python 2.7
+
+
+# Starting a game
+```
+./dod.py -h
+usage: dod.py [-h] [--logfile LOG_FILE] [--level LOG_LEVEL] num_players
+
+Play a practice session of Deduce or Die
+
+positional arguments:
+  num_players         Number of players
+
+optional arguments:
+  -h, --help          show this help message and exit
+  --logfile LOG_FILE  The name of the log file. Default=dod.log
+  --level LOG_LEVEL   Set the log level for the application log. [ERROR, WARN,
+                      INFO, DEBUG] Default=INFO
+```
+
+So to start a 4 player game you would do something like:
+```
+./dod.py 4
+```
+This would start a 4 player game and use defaults for everything else. It would produce a logfile called something like dod_20170703_140844.log
+
+# Very Brief Rules Overview
+This document assumes you're familiar with the rules of Deduce or Die. Please refer to the rules link above. A few highlights just in case you decided to skip the rules:
+* 3-6 players
+* Player Deck: One Deck of cards made up of 1-9, Diamonds, Hearts, Spades (27 cards)
+* Question Deck: One deck of cards made up two setups like the Player Deck (54 cards)
+* Deal from the Player Deck
+   * Deal two secret cards called "Evidence". Set them aside.
+   * Deal each player an equal number of cards using as many cards as possible.
+   * For all but 5 players this will leave one card left over. Expose this card to all players.
+* Each player states which suit they have the least number of cards.
+* Deal from the Question Deck
+   * Expose the top 3 cards from the question deck.
+   * The current player (always player 1 in this solitaire game) asks another player about thier hand based on these 3 cards.
+   * Repeat.
+
+
+# The Game
+
+## Start
+
+Let's start a 4 player game
+```
+./dod.py 4
+```
+
+The game starts by presenting the initial information for the game and then providing a prompt for further commands:
+```
+Exposed: 3S
+Player 1 least suit: H
+Player 2 least suit: S
+Player 3 least suit: H
+Player 4 least suit: D
+Your hand: ['1D', '7D', '1H', '1S', '4S', '6D']
+Deduce or Die
+['2D', '9S', '9S']:
+
+```
+In the above example we see that our hand has 6 cards (a 4 player game gives each player 6 cards)
+* 1 Diamonds
+* 7 Diamonds
+* 1 Hearts
+* 1 Spades
+* 4 Spades
+* 6 Diamonds
+
+We also see that the "3 Spades" has been exposed. (In a 4 player game one card is exposed).
+
+Additionally we know which suit each player has the least of. This is all the information we know at the start of the game.
+
+## The Prompt
+Following the initial information discussed above there is a prompt that looks like this:
+```
+['2D', '9S', '9S']:
+```
+
+This shows the 3 cards drawn from the question deck and is waiting for a command.
+
+## Commands
+### hand
+* parameters
+   * None.
+
+Display the players hand and other information known at the start of the game.
+
+```
+['2D', '9S', '9S']:hand
+Exposed: 3S
+Player 1 least suit: H
+Player 2 least suit: S
+Player 3 least suit: H
+Player 4 least suit: D
+Your hand: ['1D', '7D', '1H', '1S', '4S', '6D']
+```
+
+### report
+* Parameters
+   * None.
+
+Display all the questions asked and the responses
+
+```
+Ask: Player 3 | 2-9:* / Answer: 6
+Ask: Player 2 | 8-1:h / Answer: 2
+...
+```
+
+### reveal
+* Parameters
+   * None.
+
+Display all the secret information for the game.
+* Each Players hands
+* The "evidence" cards
+* The exposed card
+
+```
+['2D', '9S', '9S']:reveal
+Evidence: ['6S', '3D']
+Exposed: 3S
+Player 1: ['1D', '7D', '1H', '1S', '4S', '6D']
+Player 2: ['3H', '4D', '8D', '2H', '8H', '9H']
+Player 3: ['2D', '5H', '5S', '5D', '9S', '7H']
+Player 4: ['7S', '2S', '8S', '9D', '4H', '6H']
+```
+This command is used when you think you have deduced the hidden cards or if you're giving up.
+
+### ask
+* Parameters
+   * player number
+   * start of range (inclusive)
+   * end of range (inclusive)
+   * suit (optional)
+
+Display the number of cards the indicated player has in the range requested. If a suit is provided then the count will only include cards of that suit. If no suit is provided then the count will be for all suits.
+
+A new set of 3 cards will be dealt from the Question Deck
+
+```
+['2D', '9S', '9S']:ask 3 2 9
+Ask: Player 3 | 2-9:* / Answer: 6
+['1H', '3D', '8H']:
+```
+Above we can see the question asked is "Player 3, how many cards do you have of all suits from 2 through 9?". The answer is "6 cards"
+
+```
+['1H', '3D', '8H']:ask 2 8 1 h
+Ask: Player 2 | 8-1:h / Answer: 2
+['5H', '9H', '7H']:
+```
+Above we see the question asked is "Player 2, how many cards do you have of Hearts, 8 through 1". The answer is "2 cards". Note that the range wraps around so the range 8-1 would be [8, 9, 1]
+
+Note that there is no validation so any range can always be asked. Don't cheat.
+
+The rules allow for a special condition when an identical set of two cards are drawn, such as the set of 9s in the example above. This allows a player to ask for "all Spades". This can be done by simply asking for the entire range, i.e. ```ask 3 1 9 s```
---
+++ dod.py
@@ -0,0 +1,408 @@
+#!/usr/bin/env python
+
+#import sys
+import datetime
+import logging
+import random
+import copy
+#import shutil
+#import json
+#import re
+#import time
+import argparse
+import cmd
+
+# Shuffle the Motive deck well and place two cards face down to the side, making
+# sure that no one sees what they are. These cards are called the Evidence cards.
+# Then deal the following number of cards to each player face down:
+# 3 players - 8 cards
+# 4 players - 6 cards
+# 5 players - 5 cards
+# 6 players - 4 cards
+# All the cards are dealt out in a I've player game. Otherwise, there will be one
+# card left over. Expose this card, let everyone take note of it, and then place it
+# aside, out of the game. The players can now secretly look at the cards dealt to
+# them and make any notes they like on their deduction sheets.
+# Shuffle the Interrogation deck and place it in the center of the table.
+# Randomly choose one player to go first.
+
+DEFAULT_LOGFILE = 'dod'
+DEFAULT_LOG_LEVEL = 'INFO'
+
+log = None
+
+def setup_logging(time_now, log_level, log_path, log_filename, mode):
+
+    global log
+    global root_logger
+    global console_handler
+
+    timestamp_now = time_now.strftime('%Y%m%d_%H%M%S')
+
+    levels = {'ERROR'   :logging.ERROR,
+              'WARNING' :logging.WARNING,
+              'INFO'    :logging.INFO,
+              'DEBUG'   :logging.DEBUG}
+
+    #log_formatter = logging.Formatter('%(asctime)s %(levelname)s [%(module)s.%(funcName)s] %(message)s')
+    log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+    root_logger = logging.getLogger()
+    root_logger.setLevel(levels[log_level])
+
+    log_file = log_path + '/' + log_filename + '_' + timestamp_now + '.log'
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(log_formatter)
+    root_logger.addHandler(file_handler)
+
+    # Special logger for logs that should only emit to file
+    file_logger = logging.getLogger('file')
+    file_logger.propagate = False
+    file_logger.addHandler(file_handler)
+
+#    console_handler = logging.StreamHandler(sys.stdout)
+#    console_handler.setFormatter(log_formatter)
+#    root_logger.addHandler(console_handler)
+
+    log = logging.getLogger(mode)
+    return log_file
+
+parser = argparse.ArgumentParser(description='Play a practice session of Deduce or Die')
+
+parser.add_argument('--logfile', dest='log_file', default=DEFAULT_LOGFILE,
+                        help='The name of the log file. Default={}_<timestamp>.log'.format(DEFAULT_LOGFILE))
+parser.add_argument('--level', dest='log_level', default=DEFAULT_LOG_LEVEL,
+                        help='Set the log level for the application log. ' \
+                             '[ERROR, WARN, INFO, DEBUG] Default={}'.format(DEFAULT_LOG_LEVEL))
+parser.add_argument( 'num_players', help='Number of players (3-6)')
+
+
+args = parser.parse_args()
+
+time_now = datetime.datetime.now()
+full_log_file = setup_logging(time_now, 'DEBUG', '.', args.log_file, 'UPGRADE_PREP')
+
+
+# Cards per player
+# 3 players - 8 cards
+# 4 players - 6 cards
+# 5 players - 5 cards
+# 6 players - 4 cards
+
+cards_per_player = {3:8,
+                    4:6,
+                    5:5,
+                    6:4}
+
+
+#Create deck of cards
+suits = ['D', 'H', 'S']
+ranks = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+def create_player_deck():
+    deck = []
+    for s in suits:
+        for r in ranks:
+            card = r + s
+            deck.append(card)
+
+    return deck
+
+suit_idx = {'D' : 0,
+            'H' : 1,
+            'S' : 2}
+initial_grid = [[0, 0, 0, 0, 0, 0, 0, 0, 0], # Diamonds
+                [0, 0, 0, 0, 0, 0, 0, 0, 0], # Hearts
+                [0, 0, 0, 0, 0, 0, 0, 0, 0]] # Spades
+    
+def create_grid(hand):
+    # Create an empty grid
+    # 
+    hand_grid = copy.deepcopy(initial_grid)
+
+    # Fill the grid based on the hand
+    for c in hand:
+        #
+        # The grid is accessed by suit(x) and rank(y)
+        # Convert the suit char to an index (ignore case)
+        #
+        x = suit_idx[c[1].upper()]
+        y = int(c[0]) - 1
+        if hand_grid[x][y] != 0:
+            raise Exception("Duplicate card in hand")
+
+        hand_grid[x][y] = 1
+
+    log.info("Hand Grid: {}".format(hand_grid))
+    return hand_grid
+
+#
+#  Calculate 'least suit' for each player
+#
+def least_suit(player_grid):
+    count_map = {}
+    for s in suit_idx:
+        count = player_grid[suit_idx[s]].count(1)
+        if count_map.get(count):
+            count_map[count].append(s)
+        else:
+            count_map[count] = [s]
+
+    log.info("Count Map: {}".format(count_map))
+    least = min(count_map.keys())
+    log.info("least suit = {}/{}".format(least, count_map[least]))
+    choice = random.choice(count_map[least])
+    log.info("choice: {}".format(choice))
+    return choice
+
+
+log.info("Session Start: {players}".format(players=args.num_players))
+
+
+player_deck = create_player_deck()
+random.shuffle(player_deck)
+question_deck = player_deck + create_player_deck()
+discard_deck = []
+random.shuffle(question_deck)
+
+log.info("Player Deck: \n{}".format(player_deck))
+log.info("Question Deck:\n{}".format(question_deck))
+
+exposed = None
+hands = []
+grids = []
+questions = []
+#
+# Evidence is the first two cards of the deck
+#
+evidence = player_deck[:2]
+log.info("Evidence: {}".format(evidence))
+
+#
+# Create player hands with the rest of the deck
+#
+player_leasts = []
+start = 2
+num_players = int(args.num_players)
+for p in range(num_players):
+    end = start + cards_per_player[num_players]
+    cur_hand = player_deck[start:end]
+    start = end
+    hands.append(cur_hand)
+    grids.append(create_grid(cur_hand))
+    log.info("Player Hand: {}".format(hands[p]))
+    log.info("Player Grid: {}".format(grids[p]))
+    player_leasts.append(least_suit(grids[p]))
+
+#
+# If one card is left over then expose it.
+# This should happen for everything but 5 players
+#
+if end == 26:
+    exposed = player_deck[-1:][0]
+elif end != 27:
+    message = "Too many cards left over: {}".format(end)
+    log.error(message)
+    raise Exception(message)
+
+exposed_message = "Exposed: {}".format(exposed)
+print exposed_message
+log.info(exposed_message)
+
+for p_idx in range(num_players):
+    message = "Player {} least suit: {}".format(p_idx + 1, player_leasts[p_idx])
+    print message
+    log.info(message)
+
+print "Your hand: {}".format(hands[0])
+#
+# Show the player the exposed card (if any)
+# Show the player their hand (hand 0)
+#
+# Start the command processor. The processor should
+# always have 3 cards from the question deck exposed.
+# Possibly with all possible questions calculated as options
+#
+# Commands:
+#
+# - Ask - Ask a question based on the question deck cards.
+#         validate, answer and draw 3 new cards.
+#         Maybe this takes an option paramater if the question
+#         cards can deterministically show all possible question options.
+#
+# - Reveal - Show the evidence cards and the hands of
+#            the other players. Ends game
+# 
+# - Hand - Show the player hand and the exposed card if there is one,
+#          and the current 3 cards off the question deck.
+#
+# - Replay - Show all asked questions and answers
+#
+
+# Get the first 3 cards off the question deck
+#print "Before: {}".format(question_deck)
+#question_cards = question_deck[:3]
+#del question_deck[:3]
+#print "Question: {}".format(question_cards)
+#print "After : {}".format(question_deck)
+
+def draw_questions(q_deck, d_deck):
+    question_cards = []
+    for i in range (3):
+        if not len(q_deck):
+            #
+            # The question deck ran out of cards.
+            # shuffle over the discard deck
+            #
+            q_deck = d_deck
+            random.shuffle(q_deck)
+            d_deck = []
+            log.info("Question Deck Shuffled")
+            log.info("  Len: {}".format(len(q_deck)))
+            log.info(q_deck)
+
+        #
+        # Draw on card from the question deck
+        #
+        question_cards.append(q_deck.pop())
+
+    return (question_cards, q_deck, d_deck)
+
+def count_range(grid, start, end, s_idx):
+    player_suit = grid[s_idx]
+    if start <= end:
+        card_count = player_suit[start - 1:end].count(1)
+    else:
+        #
+        #  Create a list made of the elements before and after
+        #  the "proper" range made from the card ranks
+        #
+        card_count = (player_suit[:end] + player_suit[start-1:]).count(1)
+
+    return card_count
+
+class DodException(Exception):
+    pass
+
+class Deduce(cmd.Cmd):
+    """Play Deduce or DIe"""
+
+    def __init__(self, question_deck, discard_deck):
+        #
+        # Setup initial question cards
+        #
+        (self.question_cards, self.question_deck, self.discard_deck) = draw_questions(question_deck, discard_deck)
+        cmd.Cmd.__init__(self)
+        self.prompt = str(self.question_cards) + ':'
+
+#    question_cards = None
+#    prompt = str(question_cards) + ':'
+    intro = "Deduce or Die"
+
+    doc_header = 'doc_header'
+    misc_header = 'misc_header'
+    undoc_header = 'undoc_header'
+    
+    ruler = '-'
+
+    def ask_validate(self, parms):
+        parm_len = len(parms)
+        if parm_len < 3:
+            raise DodException("Not enough parameters")
+        if parm_len > 4:
+            print "ignoring extra parameters"
+        
+        (player, start, end) = parms[:3]
+        try:
+            player = int(player)
+            start = int(start)
+            end = int(end)
+        except ValueError:
+            raise DodException("Non-numeric value for player, start or end")
+
+        suit = None
+        s_idx = None
+        if parm_len >= 4:
+            suit = parms[3]
+            try:
+                s_idx = suit_idx[suit.upper()]
+            except KeyError:
+                raise DodException("Invalid suit")
+
+        if player > num_players:
+            raise DodException("Invalid player")
+
+        if start < 1 or start > 9:
+            raise DodException("Invalid start")
+
+        if end < 1 or end > 9:
+            raise DodException("Invalid end")
+
+        return (player, start, end, suit, s_idx)
+
+    def do_ask(self, line):
+        #
+        # ask player start_rank end_rank [suit]
+        # If suit is not provided it is assumed all suits
+        #
+        parms = line.split()
+        player = None
+        start = None
+        end = None
+        suit = None
+        s_idx = None
+        try:
+            (player, start, end, suit, s_idx) = self.ask_validate(parms)
+        except DodException as e:
+            print e.message
+            return
+
+        answer = None
+        if suit:
+            card_count = count_range(grids[player - 1], start, end, s_idx)
+            answer = "Ask: Player {} | {}-{}:{} / Answer: {}".format(player, start, end, suit, card_count)
+        else:
+            card_count =  count_range(grids[player - 1], start, end, 0)
+            card_count += count_range(grids[player - 1], start, end, 1)
+            card_count += count_range(grids[player - 1], start, end, 2)
+            answer = "Ask: Player {} | {}-{}:* / Answer: {}".format(player, start, end, card_count)
+
+        print answer
+        questions.append(answer)
+        log.info(answer)
+
+        #
+        #  Discard the old question cards.
+        #  Deal new question cards.
+        #
+        self.discard_deck += self.question_cards
+        (self.question_cards, self.question_deck, self.discard_deck) = draw_questions(self.question_deck, self.discard_deck)
+        self.prompt = str(self.question_cards) + ':'
+        log.info(str(self.question_cards))
+    
+    def do_reveal(self, line):
+        print "Evidence: {}".format(evidence)
+        print "Exposed: {}".format(exposed)
+
+        idx = 1
+        for h in hands:
+            print "Player {}: {}".format(idx, h)
+            idx += 1
+
+    def do_hand(self, line):
+        print "Exposed: {}".format(exposed)
+        for p_idx in range(num_players):
+            print "Player {} least suit: {}".format(p_idx + 1, player_leasts[p_idx])
+        print "Your hand: {}".format(hands[0])
+
+    def do_report(self, line):
+        for q in questions:
+            print q
+
+    def do_prompt(self, line):
+        "Change the interactive prompt"
+        self.prompt = line + ': '
+
+    def do_EOF(self, line):
+        return True
+
+Deduce(question_deck, discard_deck).cmdloop()
